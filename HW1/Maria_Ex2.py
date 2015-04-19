538fdf38ec1136aef9eebd2b8c50edc4bab0c41b
Maria Fernandez <maria.fernandez@barcelonagse.eu>
2015-04-19 18:42:24 +0200

    Ex 3 HW1
    First tryout of ex3, still have some problems obtaining the surname of
    the president, still have to figure out how to do it

---
+++ HW1/Maria_Ex2.py
@@ -0,0 +1,41 @@
+
+
+# Read the Data into the exercice:
+ 
+with open('sou_all.txt','r') as file_obj:
+    raw_data = file_obj.read()
+
+data = raw_data.split("**********")
+
+
+print len(data)
+
+title = [None] * (len(data)-1)
+speech =[None] * (len(data)-1)
+
+print len(title)
+print len(speech)
+
+
+## Split the main Speech of the tittle information
+for i in range (1,len(data)):
+    SpeechSplit = data[i].split("_*****")
+    title[i-1]= SpeechSplit[0]
+    speech[i-1] = SpeechSplit[1]
+    
+year = [None] * len(title)
+president =[None] * len(title)
+    
+    
+print title[len(title)-1]    
+## Split the President Name and the Year
+for i in range (0,len(title)):
+    TitleSplit = title[i].split("_")
+    year[i]= TitleSplit[1]
+    president[i] = TitleSplit[4]
+
+
+
+print year
+print president
+
