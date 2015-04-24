731f6bb2695497a37143178f8f46245c346c4565
j-verdu <januoc@gmail.com>
2015-04-24 11:28:23 +0200

    ex3 add comment ex4 refine output.to_csv

--- HW1/ex3.py
+++ HW1/ex3.py
@@ -1,27 +1,32 @@
+### Text mining for Social Sciences
+### problem set 1, ex 3
+### Maria Fernandez, Tim Kreienkamp, Joan Verdu
+
 # -*- coding: utf-8 -*-
 """
 Created on Sun Apr 19 23:06:14 2015
 
 @author: timkreienkamp
 """
+#### Import packages, load data
 import re
 import numpy as np
 import codecs
 import pandas as pd
 with codecs.open('sou_all.txt','r','utf-8') as file_obj:
 	data=file_obj.read()
- 
-
- 
-#### Second version: Better alternative
+  
+#### Extract text
 # split into headers and paragraphs directly
 pattern_paragraph=re.compile("\*\*+.?",re.UNICODE) #defined by more than 1 *
 sessions = re.split(pattern_paragraph, data)
 # note sessions[0] is void, don't use it
+
 # Go for paragraphs
-sliceObj=slice(2,len(sessions),2) #paragraphs are in odd positions
+sliceObj=slice(2,len(sessions),2) #paragraphs are in even positions
 paragraphs=sessions[sliceObj]
 paragraphs=[par.strip("\n") for par in paragraphs] # remove initial/final line breaks
+
 # Go for headers
 sliceObj=slice(1,len(sessions),2) #paragraphs are in odd positions
 headers=sessions[sliceObj]
@@ -36,16 +41,13 @@ pres_name=[re.findall(pat_pres,text)[-1] for text in headers] # last word of eac
 years = np.asarray(years, dtype = int)
 years.tolist()
 
-
-# put the data in paragraphs
+# split 'speech'-paragraphs into paragraphs delimited by \n
 i=0
 pattern_speech=re.compile("\n\n+?",re.UNICODE)
 
 yearsPar=[]
 presidentPar=[]
 SpeechPar=[]
-
-
 for speeches in paragraphs:
     paragraphsSpeech=re.split(pattern_speech, speeches)
     del paragraphsSpeech[0]
@@ -56,10 +58,8 @@ for speeches in paragraphs:
         SpeechPar.append(numpara)
     i=i+1
 
-#Put it all in the data frame
+### Wrap up in a data frame
 sou_all_frame = pd.DataFrame({'Year':yearsPar, 'President':presidentPar, 'Paragraph':SpeechPar})
     
-
- 
 #recall we had to order them chronologically
 sou_all_frame = sou_all_frame.sort("Year", ascending = False)
---
+++ HW1/ex4.py
@@ -0,0 +1,125 @@
+### Text mining for Social Sciences
+### problem set 1, ex 4
+### Maria Fernandez, Tim Kreienkamp, Joan Verdu
+
+# -*- coding: utf-8 -*-
+
+## COMMENTS
+
+## -- when we extract the date we can keep it as a string or date
+##    both ways are done in the code (we need to uncomment one to decide what to keep)
+## -- the last part with the loop of the different years, I don't know how to
+##      append the results of the different years.
+# import modules
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+
+
+## Function that returns the number of pages of that search and how many results
+def numberpages(base_url):
+    # Get the number of results/pages
+    response = requests.get(base_url)
+    soup = BeautifulSoup(response.content)
+    p_results=soup.find('p', {'class':'resultNumber'})
+    p_results = p_results.get_text()
+    pat_pres=re.compile("\xa0[0-9]+")
+    results= re.findall(pat_pres,p_results)[-1]
+    results=int(results)
+    # Number of pages assuming 10 results per page
+    docs_page=10
+    npages=results/docs_page +1
+    # subsequent pages add '&page=2', '&page=3', etc
+    pages=range(1,npages+1,1)
+    return(results,pages)
+
+## Funcion that extracts the metadata from the url
+def getdata(base_url): 
+    date=[]
+    form=[]
+    author=[]
+    text = []  
+    title=[]    
+    #get the number of results of the search and number of pages
+    results,pages=numberpages(base_url)
+    rank=range(1,results+1,1)    
+    
+    for page in pages:
+        # get next search result page
+        response = requests.get(base_url+'&page='+str(page))
+        soup = BeautifulSoup(response.content)
+        
+        #search results are organized in an html table
+        table = soup.find('table', { 'class' : 'documentTable' })
+        
+        #now we iterate through the results rowwise
+        rows = table.find_all('tr')
+        
+       #titles are at position 1, metadata is at position 3
+       
+        for i in range(0, len(rows), 3):
+            title.append(rows[i].find('td', {'class':'publicationTitle'}).get_text())
+            #filter out the metadata
+            metadata = rows[i+2]
+            # get the left metadata
+            left = metadata.find('td', {'class':'leftMetadata'})
+            
+            #get the author and date of the paper
+            right=metadata.find('ul', {'class':'metadata'}).find_all('li')
+            author.append(right[0].get_text()[8:])
+            
+            #date.append(datetime.strptime(right[1].get_text()[18:], "%d/%m/%Y" ).date())## in date format
+            date.append(right[1].get_text()[18:])## in string format        
+            form.append(left.find_all('li')[1].get_text()[6:])
+            
+            
+            # get the direct text access class
+            directtextaccess = left.find('li', {'class': 'directTextAccess'})
+            #get all the links
+            access_links = directtextaccess.find_all('a', href = True)
+            for link in access_links:
+                if '/EN/TXT/HTML/' in link['href']:
+                    #now "click" the link
+                    #print link['href']
+                    text_page = requests.get("http://eur-lex.europa.eu/"+link['href'][1:])
+                    #soupify
+                    text_page_soup = BeautifulSoup(text_page.content)
+                    text_page_body = text_page_soup.find('body')
+                    text.append(text_page_body.get_text())
+                    print len(text)
+                    break
+            else:
+                text.append('NA')
+    return(title, rank, author, date, form, text)
+    
+#### Main algorithm   
+# From the different years the only thing that changes is the end 
+# create a loop so it extracts the files of all the choosen years
+    
+base_url='http://eur-lex.europa.eu/search.html?text=palestine&scope=EURLEX&qid=1429221052950&type=quick&lang=en&DTS_SUBDOM=LEGISLATION&DD_YEAR=' 
+
+date_list =[]
+form_list =[]
+author_list =[]
+text_list = []  
+title_list =[]
+rank_list = []
+title_list = []
+   
+for i in range (2005, 2016,1):
+    
+    url=(base_url+str(i))
+    Ttitle, Trank,Tauthor,Tdate,Tform,Ttex=getdata(url)
+    rank_list.extend(Trank)
+    author_list.extend(Tauthor)
+    date_list.extend(Tdate)
+    form_list.extend(Tform)
+    text_list.extend(Ttex)
+    title_list.extend(Ttitle)    
+    
+# Wrap up the outcome
+output = pd.DataFrame({'Title':title_list, 'Date': date_list, 'Author':author_list, 'Form':form_list, 'Rank': rank_list, 'Text':text_list})
+# Export to csv
+output.to_csv('output_hw1ex4_fdez_verdu_kreienkamp.csv', sep = '\t',encoding='utf-8')
