ff8cf58203d998e670f0d798e01651fd59b28430
j-verdu <januoc@gmail.com>
2015-04-26 08:52:01 +0200

    hw2ex1

--- HW2/HW2ex1.py
+++ HW2/HW2ex1.py
@@ -9,6 +9,7 @@ Created on Fri Apr 24 13:32:58 2015
 """
 ### libraries
 import codecs, re
+import numpy as np
 from nltk.tokenize import wordpunct_tokenize
 from nltk import PorterStemmer
 
@@ -18,15 +19,16 @@ class RawDocs():
 
     def __init__(self, doc_data, stopword_file):
 
-		self.docs = [s.lower() for s in doc_data]
+        self.docs = [s.lower() for s in doc_data]
 
-		with codecs.open(stopword_file,'r','utf-8') as f: raw = f.read()
-		self.stopwords = set(raw.splitlines())
+        with codecs.open(stopword_file,'r','utf-8') as f: raw = f.read()
+        self.stopwords = set(raw.splitlines())
 
-		self.docs = map(lambda x: re.sub(u'[\u2019\']', '', x), self.docs)
+        self.docs = map(lambda x: re.sub(u'[\u2019\']', '', x), self.docs)
 
-		self.N = len(self.docs)
-		self.tokens = map(wordpunct_tokenize,self.docs)
+        self.N = len(self.docs)
+        self.tokens = map(wordpunct_tokenize,self.docs)
+           
 
 
     def token_clean(self,length):
@@ -58,6 +60,26 @@ class RawDocs():
 
 		def s(tokens): return [PorterStemmer().stem(t) for t in tokens]
 		self.stems = map(s,self.tokens)
+
+    #   NEW unique terms
+    def unique(self):
+        self.unique = set([t for d in self.tokens for t in d])
+
+    #   NEW document term matrix    
+    def doc_term(self):
+
+        def count_terms(doc,unique): #  count of doc tokens
+            # first initialize dictionary
+            my_dict=dict.fromkeys(unique,0)   
+            for i in doc: my_dict[i]+=1             
+            return my_dict.values()
+        #self.doc_term=count_terms(self.tokens[0],self.unique)
+        # Apply to tokens of each doc
+        self.doc_term=map(count_terms,self.tokens,self.unique)
+        
+    # NEW: INCIDENCE MATRIX: to be done
+    def incidence(self):
+        self.incidence=np.where(self.doc_term > 0,0,1)
     
     # NEW count method
     def count(self,dictionary):
@@ -69,10 +91,14 @@ class RawDocs():
         # Apply to every document
         self.count= map(count_doc,self.tokens)
     
-    # NEW tf_idf method
-    def tf_idf(self,dictionary):
+    # NEW tf_idf method: to be done
+ #   def tf_idf(self,dictionary):
         """
         For tokens of every document, compute td_idf according to a dictionary.
         """
         
-    
+# Sample try
+        
+import pandas as pd
+data = pd.read_table("speech_data_extend.txt",encoding="utf-8")
+docsobj = RawDocs(data.speech,'stopwords.txt')
