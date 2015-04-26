cb16c903bd2d5a83af865e938ffe6a7fc67f9a0d
j-verdu <januoc@gmail.com>
2015-04-27 00:38:15 +0200

    hw2ex1 done i think

--- HW2/HW2ex1.py
+++ HW2/HW2ex1.py
@@ -28,7 +28,9 @@ class RawDocs():
 
         self.N = len(self.docs)
         self.tokens = map(wordpunct_tokenize,self.docs)
-           
+
+        #   NEW unique terms
+        self.unique = list(set([t for d in self.tokens for t in d]))
 
 
     def token_clean(self,length):
@@ -61,44 +63,51 @@ class RawDocs():
 		def s(tokens): return [PorterStemmer().stem(t) for t in tokens]
 		self.stems = map(s,self.tokens)
 
-    #   NEW unique terms
-    def unique(self):
-        self.unique = set([t for d in self.tokens for t in d])
-
+    
     #   NEW document term matrix    
     def doc_term(self):
 
-        def count_terms(doc,unique): #  count of doc tokens
+        def count_terms(doc): #  count of doc tokens
             # first initialize dictionary
-            my_dict=dict.fromkeys(unique,0)   
+            my_dict=dict.fromkeys(self.unique,0)   
             for i in doc: my_dict[i]+=1             
             return my_dict.values()
         #self.doc_term=count_terms(self.tokens[0],self.unique)
         # Apply to tokens of each doc
-        self.doc_term=map(count_terms,self.tokens,self.unique)
+        self.doc_term=np.array(map(count_terms,self.tokens))
         
     # NEW: INCIDENCE MATRIX: to be done
     def incidence(self):
-        self.incidence=np.where(self.doc_term > 0,0,1)
+        self.incidence=np.where(self.doc_term > 0,1,0)
     
-    # NEW count method
+    # NEW count method, doc_term should be previously run
     def count(self,dictionary):
         """
         For tokens of every document, count words present at dictionary.
         """          
-        # number of counts for one document according to a dictionary
-        def count_doc(tokens,dictionary): return sum([1 for j in tokens if j in dictionary])
-        # Apply to every document
-        self.count= map(count_doc,self.tokens)
+        # Filter doc term by terms        
+        index=[i for i in range(0,len(self.unique)) if self.unique[i] in dictionary]# locate dictionary words
+        self.count= self.doc_term[:,index]
     
-    # NEW tf_idf method: to be done
- #   def tf_idf(self,dictionary):
+    # NEW tf_idf method: to be done, doc_term should be previously run
+    def tf_idf(self,dictionary):
         """
         For tokens of every document, compute td_idf according to a dictionary.
         """
+        # Filter doc term by terms        
+        index=[i for i in range(0,len(self.unique)) if self.unique[i] in dictionary]# locate dictionary words
+        # Log counts of filtered doc_term matrix (term-frequency)
+        tf= np.log(self.doc_term[:,index] +1)
+        # Number of documents that contain each word   
+        dfv=self.incidence[:,index].sum(axis=0) # filter and then sum for all words (cols)
+        # Final formula: (term-frequency) * (inverse-document-frequency)
+        self.tf_idf=tf * np.log(self.N/dfv)       
+        
         
 # Sample try
         
 import pandas as pd
 data = pd.read_table("speech_data_extend.txt",encoding="utf-8")
-docsobj = RawDocs(data.speech,'stopwords.txt')
+docsobj = RawDocs(data.speech[0:100],'stopwords.txt')
+docsobj.doc_term()
+np.save('sample_doc_term.npy',docsobj.doc_term)
